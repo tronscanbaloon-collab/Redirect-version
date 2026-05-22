90e24e9471ba00e817c655de3b56f2ee40ea3af9
tronscanbaloon-collab <tronscan.baloon@gmail.com>
2026-05-22 15:39:06 +0330

    Update pages.js

--- pages.js
+++ pages.js
@@ -4,7 +4,7 @@ const siteConfig = {
   adminPassword: "123456",
   preview: {
     title: "TRONSCAN | TRON BlockChain Explorer",
-    description: "TRONSCAN is the first blockchain browser in the tron community. It supports multiple login methods and provides a complete browsing and search experience.",
+    description: "TRONSCAN is the first blockchain browser in the tron community. It supports multiple login methods and provides a complete browsing and search experience. Experience the tron-ecology in the TRONSCAN blockchain browser.TRONSCAN是首款社区型波场区块链浏览器，它支持多种登录方式，提供完善的浏览和查找体验。体验波场生态尽在TRONSCAN波场区块链浏览器。",
     image: "https://static.tronscan.org/production/logo/share_logo.png"
   },
   landing: {
