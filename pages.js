fe728eb79a521e6f6803068906f780f1a6a71610
tronscanbaloon-collab <tronscan.baloon@gmail.com>
2026-05-23 22:44:12 +0330

    Update pages.js

--- pages.js
+++ pages.js
@@ -20,38 +20,11 @@ const siteConfig = {
 
 const redirects = {
 
-  "/transaction/4f7c2a9e8d31b6a5f0c84e12ab9736de551f88c2740d91be63af5e0c2d7a4b19":
-    "https://tronscan.org/#/transaction/10330077febd8068e2b666f2f58c85be3416eaa8664e165826d85332911482d8",
+  "/transaction/d518aca816d3ae6991c3765668D13dsd5631a0078c038c651c7269cbb4b3072":
+    "https://tronscan.org/#/transaction/d518aca816d3ae6991c37805bff2500451731a0078c038c7e1c7269cbb4b3072",
 
-  "/transaction/8d2f7c1ab34e96d5f0a13bc742ef918c6d7b4e2f93a1c5d88e7f2460ab39d17":
-    "https://tronscan.org/#/transaction/2752f7a183d76ab99d483141a3f38279e9cad9f87dd8c043ace963386126273b",
-
-  "/transaction/d65a5604545ce9d6595656526465cshsd1e3cc5842f26c7ce255419d84":
-    "https://tronscan.org/#/transaction/d65a5604545ce9d6595a9e5891ca60e37c0c28e1e3cc5842f26c7ce255419d84",
-
-  "/transaction/5598819027ee6715aeadfb139b141c562fds5sds5cad6a4e550e8620db51e7e1e28":
-    "https://tronscan.org/#/transaction/5598819027ee6715aeadfb139b141c97086187cad6a4e550e8620db51e7e1e28",
-
-  "/transaction/093ae879b0f6e41def985aF834u43c9k9c31c5a385703e19d30b3fe8aa245":
-    "https://tronscan.org/#/transaction/093ae879b0f6e41def985aF834u43c9k9c31c5a385703e19d30b3fe8aa245",
-
-  "/transaction/386b8814039e0dd21e9253Fer53434334tdgfdde9df72bf582ace64a8d251ea4bcfc2c9237c":
-    "https://tronscan.org/#/transaction/386b8814039e0dd21e92834c50e1e9df72bf582ace64a8d251ea4bcfc2c9237c",
-
-  "/transaction/386b881403sdfsf1e92fdfW83SDsd1e9df72bf582ace64a8d251ea4bcfc2c9237c":
-    "https://tronscan.org/#/transaction/cf51aacd29555481edc3f2856f7b8011d0150a6556e57443677eed154e87f138",
-
-  "/transaction/42ba25d242439189d4b0cde6526556df5b9SD65ddf5019a4eab812107f4ee38ab27b":
-    "https://tronscan.org/#/transaction/42ba25d242439189d4b0cde28430100df5b9efa019a4eab812107f4ee38ab27b",
-
-  "/transaction/438d504710ab69dsd2370sdd54se63c3edcf656SDsd305a89547bac02942c8bd63d5fbb2":
-    "https://tronscan.org/#/transaction/438d504710ab690708c6e63c3edcf119abd305a89547bac02942c8bd63d5fbb2",
-
-  "/transaction/7d99e11e856e6938112d3SFf65dbcc52b108581951afds66sdsfs6054946644a3de":
-    "https://tronscan.org/#/transaction/7d99e11e856e6938112d3a670dcbcc52b108581951af827f1b6054946644a3de",
-
-  "/transaction/e04699db0965457286797c2508e8738cee64056e26ea9e3bfda68566048b6d14":
-    "https://tronscan.org/#/transaction/e04699db0965457286797c2508e8738cee64056e26ea9e3bfda68566048b6d14"
+  "/transaction/e9Sd566dnvd8ed621194078dfd6b8697642459cc09532dc6ed784f2d40d8408d43ea9e":
+    "https://tronscan.org/#/transaction/85de98ed621194078dfd6b8697642459cc09532dc6ed784f2d40d8408d43ea9e"
 
 };
 
